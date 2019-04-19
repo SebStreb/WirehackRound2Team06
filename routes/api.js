3c9783edf37aa0b51cf67a80634810561410bbea
fdardenne <dardenneflorent@outlook.com>
2019-04-19 23:11:42 +0200

    api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+// Users
+router.get('/users', function(req, res, next) {
+    res.render('credits/index');
+});
 
+router.put('/user/create', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.post('/edit/:id_user', function(req, res, next) {
+    //req.params.id_user
+    res.render('credits/index');
+});
+
+router.get('/user/:id_user', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.get('/user/loan/:id_user', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.get('/user/credit/:id_user', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.get('/user/available/:id_user', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.get('/user/available/:id_user', function(req, res, next) {
+    res.render('credits/index');
+});
+
+//Loan
+router.get('/loans', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.get('/loan/:id_loan', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.post('/loan/edit/:id', function(req, res, next) {
+    res.render('credits/index');
+});
+
+router.put('/loan/:create', function(req, res, next) {
+    res.render('credits/index');
+});
+
+// 
+
+
+router.get('/user/available/:id_user', function(req, res, next) {
+    res.render('credits/index');
 
 module.exports = router;
