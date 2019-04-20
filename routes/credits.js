feb1fb3aa649082e862e45b5a4beaf6b9e87fbb6
CelineDknp <celine.deknop@student.uclouvain.be>
2019-04-20 09:15:06 +0200

    credits are dynamic

--- routes/credits.js
+++ routes/credits.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 router.get('/', function(req, res, next) {
-    res.render('credits/index');
+    if(!req.user)
+		res.render("Please connect")
+	request('http://localhost:3000/api/user/loan/'+req.user.id, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+    	console.log(response.body)
+        res.render('credits/index', {
+        	credits:JSON.parse(response.body),
+        	user:req.user
+        });
+     }
+     if(error){
+     	console.log(error)
+     }
+})
 });
 
 router.get('/show/:id', function(req, res, next) {
